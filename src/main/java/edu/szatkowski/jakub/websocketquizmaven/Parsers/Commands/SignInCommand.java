bac8525238c63721a9736b6ad3961fd238004785
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2019-06-30 14:56:54 +0200

    Updated SignIn and SignOut Command to use ResponseGenerator

--- src/main/java/edu/szatkowski/jakub/websocketquizmaven/Parsers/Commands/SignInCommand.java
+++ src/main/java/edu/szatkowski/jakub/websocketquizmaven/Parsers/Commands/SignInCommand.java
@@ -5,9 +5,15 @@
  */
 package edu.szatkowski.jakub.websocketquizmaven.Parsers.Commands;
 
+import edu.szatkowski.jakub.websocketquizmaven.Helpers.Enums.StatementType;
+import edu.szatkowski.jakub.websocketquizmaven.Helpers.ResponseGenerator;
 import edu.szatkowski.jakub.websocketquizmaven.Managers.AccountManager;
 import edu.szatkowski.jakub.websocketquizmaven.Managers.GameManager;
 import edu.szatkowski.jakub.websocketquizmaven.Parsers.Commands.Abstract.ICommand;
+import edu.szatkowski.jakub.websocketquizmaven.Responses.SignInResponse;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.websocket.Session;
 
 /**
@@ -20,7 +26,18 @@ public class SignInCommand implements ICommand{
     
     @Override
     public void execute(Session session, AccountManager accountManager, GameManager gameManager) {
-        accountManager.signIn(session, username, password);
+        ResponseGenerator responseGenerator = new ResponseGenerator();
+        StatementType result = accountManager.signIn(session, username, password);
+        try {
+            if (result == StatementType.SignInSuccessful) {
+                SignInResponse response = new SignInResponse(true);
+                session.getBasicRemote().sendText(responseGenerator.generateResponse(response));
+                return;
+            }
+            session.getBasicRemote().sendText(responseGenerator.generateErrorResponse(result));
+        } catch (IOException ex) {
+            Logger.getLogger(SignInCommand.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
--- src/main/java/edu/szatkowski/jakub/websocketquizmaven/Parsers/Commands/SignOutCommand.java
+++ src/main/java/edu/szatkowski/jakub/websocketquizmaven/Parsers/Commands/SignOutCommand.java
@@ -5,9 +5,15 @@
  */
 package edu.szatkowski.jakub.websocketquizmaven.Parsers.Commands;
 
+import edu.szatkowski.jakub.websocketquizmaven.Helpers.Enums.StatementType;
+import edu.szatkowski.jakub.websocketquizmaven.Helpers.ResponseGenerator;
 import edu.szatkowski.jakub.websocketquizmaven.Managers.AccountManager;
 import edu.szatkowski.jakub.websocketquizmaven.Managers.GameManager;
 import edu.szatkowski.jakub.websocketquizmaven.Parsers.Commands.Abstract.ICommand;
+import edu.szatkowski.jakub.websocketquizmaven.Responses.SignOutResponse;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.websocket.Session;
 
 /**
@@ -18,7 +24,20 @@ public class SignOutCommand implements ICommand{
 
     @Override
     public void execute(Session session, AccountManager accountManager, GameManager gameManager) {
-        accountManager.signOut(session);
+        ResponseGenerator responseGenerator = new ResponseGenerator();
+        StatementType result = accountManager.signOut(session);
+        try {
+            if(result == StatementType.SignOutSuccessful)
+            {
+                SignOutResponse response = new SignOutResponse(false);
+                session.getBasicRemote().sendText(responseGenerator.generateResponse(response));
+                return;
+            }
+        
+            session.getBasicRemote().sendText(responseGenerator.generateErrorResponse(result));
+        } catch (IOException ex) {
+            Logger.getLogger(SignOutCommand.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
