13fe0dde429ea3b784f1ba3d26de68067e47bc21
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2019-06-30 14:41:11 +0200

    Refactored games Array to HashMap and added possibilty to remove games and get all games

--- src/main/java/edu/szatkowski/jakub/websocketquizmaven/Managers/GameManager.java
+++ src/main/java/edu/szatkowski/jakub/websocketquizmaven/Managers/GameManager.java
@@ -6,46 +6,48 @@
 package edu.szatkowski.jakub.websocketquizmaven.Managers;
 
 import edu.szatkowski.jakub.websocketquizmaven.Models.Game;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.HashMap;
 import java.util.Random;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  *
  * @author Szatku
  */
 public class GameManager {
-    private List<Game> games;
+    private HashMap<Integer, Game> games;
     
     public GameManager() {
-        games = new ArrayList<>();
+        games = new HashMap<>();
     }
     
     public int createGame()
     {
         Random rand = new Random();
-        List<Integer> notAvailablePins = games.stream().map(game -> game.getPin()).collect(Collectors.toList());
         for(int i = 0; i < 100; i++)
         {
             int generatedPin = rand.nextInt((999999 - 100000) + 1) + 100000;
-            if(!notAvailablePins.contains(generatedPin))
+            if(!this.games.containsKey(generatedPin))
             {
                 Game newGame = new Game(generatedPin);
-                games.add(newGame);
+                games.put(generatedPin, newGame);
                 return generatedPin;
             }
         }
         return -1;
     }
     
+    public HashMap<Integer, Game> getGames()
+    {
+        return this.games;
+    }
+    
     public Game getGame(int pin)
     {
-        Optional<Game> opt = games.stream().filter(game -> game.getPin() == pin).findFirst();
-        if(opt.isPresent())
-            return opt.get();
-        return null;
+        return this.games.getOrDefault(pin, null);
+    }
+    
+    public boolean removeGame(int pin)
+    {
+        return this.games.remove(pin) != null;
     }
 }
