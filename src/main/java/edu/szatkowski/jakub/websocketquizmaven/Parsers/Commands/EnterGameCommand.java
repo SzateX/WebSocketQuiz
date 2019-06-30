960edef8d8cee7f86e28b6980817b0fe3c8b86f0
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2019-06-30 14:58:17 +0200

    Added GameManager Commands

---
+++ src/main/java/edu/szatkowski/jakub/websocketquizmaven/Parsers/Commands/EnterGameCommand.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.szatkowski.jakub.websocketquizmaven.Parsers.Commands;
+
+import edu.szatkowski.jakub.websocketquizmaven.Helpers.Enums.StatementType;
+import edu.szatkowski.jakub.websocketquizmaven.Helpers.ResponseGenerator;
+import edu.szatkowski.jakub.websocketquizmaven.Managers.AccountManager;
+import edu.szatkowski.jakub.websocketquizmaven.Managers.GameManager;
+import edu.szatkowski.jakub.websocketquizmaven.Models.Game;
+import edu.szatkowski.jakub.websocketquizmaven.Parsers.Commands.Abstract.ICommand;
+import edu.szatkowski.jakub.websocketquizmaven.Responses.PlayerAddedResponse;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.websocket.Session;
+
+/**
+ *
+ * @author Jakub.Szatkowski.2
+ */
+public class EnterGameCommand implements ICommand{
+    private int pin;
+    
+    @Override
+    public void execute(Session session, AccountManager accountManager, GameManager gameManager) {
+        ResponseGenerator responseGenerator = new ResponseGenerator();
+        Game game = gameManager.getGame(pin);
+        try{
+            if(game == null)
+            {
+                session.getBasicRemote().sendText(responseGenerator.generateErrorResponse(StatementType.GameNotExist));
+                return;
+            }
+            game.addPlayer(session);
+            PlayerAddedResponse response = new PlayerAddedResponse(true);
+            session.getBasicRemote().sendText(responseGenerator.generateResponse(response));
+        }
+        catch (IOException ex)
+        {
+            Logger.getLogger(CreateGameCommand.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+}
---
+++ src/main/java/edu/szatkowski/jakub/websocketquizmaven/Parsers/Commands/GetGamesCommand.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.szatkowski.jakub.websocketquizmaven.Parsers.Commands;
+
+import edu.szatkowski.jakub.websocketquizmaven.Helpers.Enums.StatementType;
+import edu.szatkowski.jakub.websocketquizmaven.Helpers.ResponseGenerator;
+import edu.szatkowski.jakub.websocketquizmaven.Managers.AccountManager;
+import edu.szatkowski.jakub.websocketquizmaven.Managers.GameManager;
+import edu.szatkowski.jakub.websocketquizmaven.Models.Game;
+import edu.szatkowski.jakub.websocketquizmaven.Parsers.Commands.Abstract.ICommand;
+import edu.szatkowski.jakub.websocketquizmaven.Responses.GetGamesResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.websocket.Session;
+
+/**
+ *
+ * @author Jakub.Szatkowski.2
+ */
+public class GetGamesCommand implements ICommand{
+
+    @Override
+    public void execute(Session session, AccountManager accountManager, GameManager gameManager) {
+        ResponseGenerator responseGenerator = new ResponseGenerator();
+        try{
+            if(!accountManager.isLoggedIn(session))
+            {
+                session.getBasicRemote().sendText(responseGenerator.generateErrorResponse(StatementType.NotLoggedIn));
+                return;
+            }
+            HashMap<Integer, Game> games = gameManager.getGames();
+            GetGamesResponse response = new GetGamesResponse(games);
+            session.getBasicRemote().sendText(responseGenerator.generateResponse(response));
+        }
+        catch (IOException ex)
+        {
+            Logger.getLogger(CreateGameCommand.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+}
